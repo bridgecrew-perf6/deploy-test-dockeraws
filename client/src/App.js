8996afcd94e4cef7ca140683e38fd7e97eeef58e
Rodrigo Matos da Silva <orodrigomatos@gmail.com>
2022-05-01 23:38:18 -0300

    basico front

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import GlobalStyle from "./GlobalStyle";
+import HomePage from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { MainContainer } from "./style";
+
+
+export default function HomePage() {
+    return (
+        <MainContainer>
+            <h1>Geromel</h1>
+        </MainContainer>
+    )
+}
